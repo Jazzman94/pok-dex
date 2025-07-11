335e9fcb98e8cfdc8374ae6bce22eb1b768a5845
Jiří Jaskowiec <jirijaskowiec@seznam.cz>
2025-07-11 11:32:51 +0200

    add  help and exit commands to REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,26 +1,48 @@
 import { createInterface } from "readline";
-
-export function cleanInput(input: string): string[] {
-  return input.trim().toLowerCase().split(/\s+/);
-}
+import { getCommands } from "./commands.js";
 
 export function startREPL() {
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: 'Pokedex > '
-        });
-    
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+
+    const commands = getCommands();
+    const cmd = commands[commandName];
+    if (!cmd) {
+      console.log(
+        `Unknown command: "${commandName}". Type "help" for a list of commands.`,
+      );
+      rl.prompt();
+      return;
+    }
+
+    try {
+      cmd.callback(commands);
+    } catch (e) {
+      console.log(e);
+    }
+
     rl.prompt();
-    
-    rl.on('line', (line) => {
-        const input = cleanInput(line);
-        console.log(`Your command was: ${input[0]}`)
-    rl.prompt();
-    }).on('close', () => {
-    console.log('Have a great day!');
-    process.exit(0);
-    });
+  });
 }
 
-
+export function cleanInput(input: string): string[] {
+  return input
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
+}
